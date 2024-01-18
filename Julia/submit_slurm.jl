77b25e3116138b61f732bc9cfcc589066922acd6
bidhya <yadav.111@osu.edu>
2024-01-18 09:16:40 -0500

    add Milan node for Discover slurm jobs

--- Julia/submit_slurm.jl
+++ Julia/submit_slurm.jl
@@ -54,7 +54,7 @@ function create_job(hpc, jobname, cores, memory, runtime, out_subfolder, start_i
         # fh.writelines('#SBATCH --constraint="[cas|sky]"\n')
         if hpc == "discover"
             write(f, "#SBATCH --account=s2701\n")
-            write(f, """#SBATCH --constraint="[cas|sky]"\n""")
+            write(f, """#SBATCH --constraint="[mil|cas|sky]"\n""")
             if runtime > 12
                 write(f, "#SBATCH --qos=long\n")  # for jobs 12-24 hours.
                 if runtime > 24
@@ -141,7 +141,7 @@ for i in StepRange(1, step, szY)
     valid_pix_ind = findall(!ismissing, B)
     valid_pix_count = length(valid_pix_ind)
     # estimate runtime as function of the number of cores used. 210 seconds = 3.5 mins; ie 1 pixel processing time ~ 0.3 min.
-    runtime = Int(ceil(valid_pix_count/(cores*150)))  #120; 210; with exclusive, how many cores we get is not certain, but just an estimate
+    runtime = Int(ceil(valid_pix_count/(cores*180)))  # 150 120; 210; with exclusive, how many cores we get is not certain, but just an estimate
     global total_runtime += runtime
     # 240 seconds : timeout error on OSC, so redude time
     # runtime = "08:00:00"  # 12 "24:00:00"  # default (max for Discover)
