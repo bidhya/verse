95fa1497c2bc447be8cbe1857107da39c118cd67
bidhya <yadav.111@osu.edu>
2024-05-18 14:41:06 -0400

    submit blender job using v17 or 1km runs

--- Julia/submit_slurm.jl
+++ Julia/submit_slurm.jl
@@ -1,9 +1,7 @@
-"""
-USAGE: pass Water Year and stepsize (number of rows)
-- julia ../verse/Julia/submit_slurm.jl 2016 35
-
-Create and submit Blender job using Julia
-
+""" Create and submit Julia Blender jobs to Slurm on Discover, OSC HPCs.
+    USAGE: pass Water Year and stepsize (number of rows)
+    julia ../verse/Julia/submit_slurm.jl 2016 10 010
+    julia ../verse/Julia/submit_slurm.jl 2016 100 050
 """
 arg_len = length(ARGS)
 out_subfolder = ARGS[1]  # WY2016. output subfolder relative to input files; temp_nc saved here
@@ -12,23 +10,31 @@ water_year = out_subfolder[end-3:end] #last 4 chars are assumed year, else error
 step = ARGS[2] #|| 35 on discover
 step = parse(Int32, step)
 memory = "184gb"
+# Grid Resolution: 050 = 0.050 degree (5km); 025 = 0.025 degree; 010 = 0.01 degree; 001 = 0.001 degree (100 meters) etc
+RES = ARGS[3] # "050"
 
 # mkpath(logDir)
-mkpath("slurm_jobs/$(water_year)/.out")
-cd("slurm_jobs/$(water_year)")
+mkpath("slurm_jobs/$(RES)/$(water_year)/.out")
+cd("slurm_jobs/$(RES)/$(water_year)")
 
 # 1. Setup inputs and output directories and file locations
 # select the root directory (this will be different on windows, linux or Mac) 
 host_machine = gethostname()
 if occursin("discover", host_machine) #|| occursin("borg", host_machine)
-    root_dir = "/discover/nobackup/projects/coressd"
-    base_folder = "$root_dir/Blender"
+    root_dir = "/discover/nobackup"
+    base_folder = "$root_dir/projects/coressd/Blender"
+    # base_folder = "$root_dir/byadav/coressd/Blender"  # aside: temporary when main storage was full.
+    DataDir = "$root_dir/projects/coressd/Blender/Inputs_$(RES)"  # INDIR. must exist  (Old = NoahMP)
     hpc_name = "discover"
-    cores = 110  # 125 46 or use 45 so one core can be used to monitor run using srun/htop.
+    cores = 110  # 110 for stepsize 125 for 5 km run.
+    if RES == "010"
+        cores = 80  # 60. use less cores to prevent NODE_FAIL error.
+    end
     memory = "0" #"184gb"
 elseif occursin(".osc.edu", host_machine)
     root_dir = "/fs/ess/PAS1785/coressd"  # "/fs/scratch/PAS1785/coressd"
     base_folder = "$root_dir/Blender"
+    DataDir = "$base_folder/Inputs_$(RES)"
     hpc_name = "osc"
     cores = 48  # 40
     memory = "0" #"175gb"
@@ -36,6 +42,7 @@ elseif occursin("asc.ohio-state.edu", host_machine)  # .unity
     root_dir = "/fs/project/howat.4/yadav.111/coressd"  # homedir()  #  Unity
     # base_folder = "/home/yadav.111/Github/Blender"  # old
     base_folder = "$root_dir/Blender"  # "$root_dir/Github/coressd/Blender"
+    DataDir = "$base_folder/Inputs_$(RES)"
     hpc_name = "unity"
     cores = 40  # 39 24 cores with 96GB memory for old node
     memory = "0"  # 186gb seems max allowed
@@ -73,7 +80,7 @@ function create_job(hpc, jobname, cores, memory, runtime, out_subfolder, start_i
         write(f, "#SBATCH --nodes=1 --ntasks=$(cores)\n")  # 
         write(f, "#SBATCH --exclusive\n")  #  use whole node with all cores without sharing 
         write(f, "#SBATCH --mem=$(memory)\n")  # zero implies use all available memory
-        write(f, "#SBATCH --mail-type=ALL\n")
+        write(f, "#SBATCH --mail-type=END,FAIL\n")  #   ALL; BEGIN,END,FAIL,REQUEUE,TIME_LIMIT_90 (reached 90 percent of time limit)
         write(f, "#SBATCH --mail-user=yadav.111@osu.edu\n")
         # Delay slurm job start by a few minutes so that the memory related problem of all nodes reading the same input nc file is resolved  
         write(f, "#SBATCH --begin=now+$(begin_delay)minutes\n")  ## minute, hour
@@ -95,30 +102,34 @@ function create_job(hpc, jobname, cores, memory, runtime, out_subfolder, start_i
         else
             write(f, "cp -r ~/Github/verse .\n")
         end
-        write(f, "julia verse/Julia/call_Blender_v16.jl $(out_subfolder) $(start_idx) $(end_idx)\n\n")
+        write(f, "julia verse/Julia/call_Blender_v17.jl $(out_subfolder) $(start_idx) $(end_idx) $(RES)\n\n")
         write(f, "squeue --job \$SLURM_JOBID \n")
         write(f, "echo List of files on TMPDIR\n")
         write(f, "echo ---------------------------------------------------------------------\n")
         write(f, "ls -ltrh\n")
         write(f, "echo -n Count of outputs_txt files: ; ls outputs_txt_*|wc -l\n")  # more specific: outputs_txt_$(start_idx)_$(end_idx)
         # write(f, "echo -n Logs: ; ls logs_$(start_idx)_$(end_idx)|wc -l\n")  # logs_* only can be a bit misleading because it will match other files with logs prefix, so be explicit.  
-
-        write(f, "tar -czf logs_$(start_idx)_$(end_idx).tar.gz logs*\n")  # list log files on node
-        write(f, "cp logs_$(start_idx)_$(end_idx).tar.gz $base_folder/Runs/$out_subfolder/logs\n")  # folder created at end of call_Blender script  
+        write(f, "ECODE=\$?\n")
+        write(f, "if [ \$ECODE -eq 0 ]; then\n")
+        write(f, "\ttar -czf logs_$(start_idx)_$(end_idx).tar.gz logs*\n")  # list log files on node
+        write(f, "\tcp logs_$(start_idx)_$(end_idx).tar.gz $base_folder/Runs/$(RES)/$out_subfolder/logs\n")  # folder created at end of call_Blender script  
         # write(f, "cp logs_$(start_idx)_$(end_idx).tar.gz \$SLURM_SUBMIT_DIR\n")
         # TODO Write/Call a python script (or Jupyter notebook) to QA/QC the run: count, tables, figures.  
-        write(f, "echo Finished Slurm job \n\n")
+        write(f, "\techo Finished Slurm job successfully\n\n")
+        write(f, "else\n")
+        write(f, "\techo Error in Blender run\n")
+        write(f, "fi\n")
     end
     run(`sbatch $(job_file)`)  # submit the job
 end
 
 using Rasters, NCDatasets
-DataDir = "$base_folder/Inputs"  # must exist. Old --> NoahMP
+# DataDir = "$base_folder/Inputs"  # must exist. Old --> NoahMP
 
 # 2. Read the Input netCDF file
 A = RasterStack("$(DataDir)/WY_merged/$(water_year)_seup_modis.nc", lazy=true)
 A = A[:SWE_tavg][Ti=1];
-szY = size(A, 2)  # get size in Y-dimension; here dim = 2
+szY = size(A, 2)  # get size in Y-dimension; here dim = 2. 6500 for 1km run.
 # szY = 10 # only for debug and testing, use smaller number
 job_count = 0
 delay_multiplier = 2  # delay the consecutive slurm job by ~2 minutes
@@ -126,7 +137,7 @@ if occursin("asc.ohio-state.edu", host_machine)
     delay_multiplier = 5  # longer delay on unity becuase of slow speeds in moving data (network related)
 end
 total_runtime = 0
-# for i in StepRange(501, step, 600)  # for testing
+# for i in StepRange(3001, step, 3003)  # for testing
 for i in StepRange(1, step, szY)
     start_idx = i
     end_idx = start_idx + step - 1
@@ -153,7 +164,7 @@ for i in StepRange(1, step, szY)
     valid_pix_count = length(valid_pix_ind)
     # estimate runtime as function of the number of cores used. 270 seconds = 4.5 mins; ie 1 pixel processing time ~ 0.3 min.
     runtime = Int(ceil(valid_pix_count/(cores*420)))  # hours. 270 for v_15=180; with exclusive, how many cores we get is not certain, but just an estimate
-    runtime = max(runtime, 11)  # bump smaller runtime to 11 hours. Due to timeout errors for short runs.   
+    runtime = max(runtime, 12)  # bump smaller runtime to 12 hours (max allowed by default on Discover). Due to timeout errors for short runs.   
     # if runtime < 10 && hpc_name=="discover"
     #     # keep getting runtime errors when less number of pixels are processed.
     #     # say in less than 4 hours, likely all cores are not being utilized efficiently
