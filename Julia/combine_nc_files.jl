ec6850b4f826df6bf5ba59426b270daab4b0b61d
bidhya <yadav.111@osu.edu>
2024-05-18 14:43:37 -0400

    combine nc files for 1 km run

--- Julia/combine_nc_files.jl
+++ Julia/combine_nc_files.jl
@@ -12,6 +12,8 @@ parallelized using threads
 arg_len = length(ARGS)
 out_subfolder = ARGS[1]  # WY2016. output subfolder relative to input files; temp_text and nc_outputs saved here
 water_year = out_subfolder[end-3:end] #last 4 chars are assumed year, else error. out_subfolder[3:end]
+RES = ARGS[2] # "050"  # Grid Resolution: 050 = 0.050 degree (5km); 025 = 0.025 degree; 010 = 0.01 degree; 001 = 0.001 degree (100 meters) etc
+
 
 using Logging, LoggingExtras
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -71,40 +73,8 @@ using Rasters, NCDatasets #, ArchGDAL
 
 # base_folder = "$root_dir/Github/Blender"
 # DataDir= "$base_folder/nc_files"
-DataDir = "$base_folder/Inputs"  # must exist (Old = NoahMP)
-
-# # # 2. Read the Input netCDF file
-# # A = RasterStack("$DataDir/WY_merged/2016_clip_noahmp_modscag.nc")  #, mappedcrs=EPSG(4326); for NoahMP with MODSCAG mapped to NoahMP resolution
-# # Following check are for prototyping only when running code locally, because I do not yet have NorthAmerica netcdf file
-# if occursin("L-JY0R5X3", host_machine) #|| test_run  # use this for testing as well
-#     @info("Test Run only.")
-#     # A = RasterStack("$DataDir/WY_merged/2016_clip_noahmp_cgf.nc", lazy=true)  # Error now (Aug 15, 2023). maybe it is old file/format
-#     A = RasterStack("$(DataDir)/WY_merged/$(water_year)_seup_modis.nc", lazy=true)
-#     # subset = A[X(Between(-120.5, -120)), Y(Between(60, 60.5))]  # 10 by 10 pixels; use small chip for prototyping
-#     # subset_fname = "$(DataDir)/WY_merged/subset_$(water_year)_seup_modis.nc"
-#     # # isfile(subset_fname) || write(subset_fname, subset)  # save. we need for post-processing analysis of results
-#     # write(subset_fname, subset)
-#     # A = RasterStack(subset_fname, lazy=true)  # read again (why). for consistency
-# # elseif occursin("borg", host_machine)  # TODO: discover
-# #     # A = RasterStack("$DataDir/WY_merged/2013_seup_modis.nc")  # 2016_noahmp_cgf 2016_clip_noahmp_cgf #, mappedcrs=EPSG(4326); for NoahMP with MODSCAG mapped to NoahMP resolution
-# #     A = RasterStack("$DataDir/WY_merged/" * water_year * "_seup_modis.nc")
-# # elseif occursin(".osc.edu", host_machine)
-# #     # A = RasterStack("$DataDir/WY_merged/2016_clip_noahmp_cgf.nc")
-# #     A = RasterStack("$DataDir/WY_merged/" * water_year * "_seup_modis.nc")
-# # elseif occursin("asc.ohio-state.edu", host_machine)
-# #     A = RasterStack("$DataDir/WY_merged/" * water_year * "_seup_modis.nc")
-# else
-#     # A = RasterStack("$DataDir/WY_merged/2016_seup_modis.nc")
-#     # A = RasterStack("$DataDir/WY_merged/" * water_year * "_seup_modis.nc")
-#     cp("$DataDir/WY_merged/$water_year" * "_seup_modis.nc", "$tmpdir/$water_year" * "_seup_modis.nc")  # copy to local machine
-#     A = RasterStack("$tmpdir/$water_year" * "_seup_modis.nc")  ## , lazy=true https://github.com/rafaqz/Rasters.jl/issues/449
-#     # if test_run
-#     #     # Aside: Get test set of data
-#     #     A = A[X(Between(-130, -120)), Y(Between(60, 65))]
-#     #     subset_fname = "$(DataDir)/WY_merged/subset_$(water_year)_seup_modis.nc"
-#     #     write(subset_fname, A)
-#     # end
-# end
+DataDir = "$base_folder/Inputs_$(RES)"  # must exist (Old = NoahMP)
+# DataDir = "$root_dir/projects/coressd/Blender/Inputs_$(RES)"  # INDIR. must exist  (Old = NoahMP)
 
 start_time = time_ns()
 
@@ -117,19 +87,7 @@ outRasterTemplate[:,:,:] .= missing
 A = nothing
 GC.gc()
 
-# SWERaster = copy(A[:SWE_tavg])
-# SWERaster[:,:,:] .= missing
-# GmeltRaster = deepcopy(SWERaster)  # use deepcopy; simple copy will create general array, not a sharedarray, hence, won't work in parallel  
-# GRaster = deepcopy(SWERaster)
-# PrecipRaster = deepcopy(SWERaster)
-# UsRaster = deepcopy(SWERaster)
-# GpvRaster = deepcopy(SWERaster)
-# GmeltpvRaster = deepcopy(SWERaster)
-# UpvRaster = deepcopy(SWERaster)
-# SWEpvRaster = deepcopy(SWERaster);
-
-
-nc_outDir = "$base_folder/Runs/$out_subfolder/temp_nc"  # /outputs_$(start_idx)_$(end_idx)
+nc_outDir = "$base_folder/Runs/$(RES)/$out_subfolder/temp_nc"  # /outputs_$(start_idx)_$(end_idx)
 
 function combine_nc(nc_outDir, var)
     # Combine nc files
@@ -147,7 +105,7 @@ function combine_nc(nc_outDir, var)
     return outRaster
 end
 
-final_nc_outDir = "$base_folder/Runs/$out_subfolder/outputs"
+final_nc_outDir = "$base_folder/Runs/$(RES)/$out_subfolder/outputs"
 mkpath(final_nc_outDir)
 out_vars = ("SWE", "Gmelt", "G", "Precip", "Us", "Gpv", "Gmeltpv", "Upv", "SWEpv")
 for var in out_vars
@@ -164,63 +122,6 @@ for var in out_vars
         @info("$var  NC file alreay processed before, skipping")
     end
 end
-# exit(0)
-
-
-# folders = readdir(nc_outDir)
-# Threads.@threads for folder in folders
-#     @info("Processing nc_file: $folder")
-
-#     B = RasterStack("$(nc_outDir)/$(folder)/SWE.nc", lazy=true)
-#     SWERaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:SWE]
-
-#     B = RasterStack("$(nc_outDir)/$(folder)/Gmelt.nc", lazy=true)
-#     GmeltRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:Gmelt]
-
-#     B = RasterStack("$(nc_outDir)/$(folder)/G.nc", lazy=true)
-#     GRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:G]
-
-#     B = RasterStack("$(nc_outDir)/$(folder)/Precip.nc", lazy=true)
-#     PrecipRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:Precip]
-    
-#     B = RasterStack("$(nc_outDir)/$(folder)/Us.nc", lazy=true)
-#     UsRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:Us]
-    
-#     B = RasterStack("$(nc_outDir)/$(folder)/Gpv.nc", lazy=true)
-#     GpvRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:Gpv]
-    
-#     B = RasterStack("$(nc_outDir)/$(folder)/Gmeltpv.nc", lazy=true)
-#     GmeltpvRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:Gmeltpv]
-    
-#     B = RasterStack("$(nc_outDir)/$(folder)/Upv.nc", lazy=true)
-#     UpvRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:Upv]
-    
-#     B = RasterStack("$(nc_outDir)/$(folder)/SWEpv.nc", lazy=true)
-#     SWEpvRaster[At(lookup(B, X)), At(lookup(B, Y)), :] = B[:SWEpv]    
-# end
-
-# nc_outDir = "$base_folder/Runs/$out_subfolder/outputs"
-# mkpath(nc_outDir)
-# @info("nc_outDir : $nc_outDir")
-
-# SWERaster = rebuild(SWERaster; name=:SWE)
-# write("$nc_outDir/SWE.nc", SWERaster)
-# GmeltRaster = rebuild(GmeltRaster; name=:Gmelt)
-# write("$nc_outDir/Gmelt.nc", GmeltRaster)
-# GRaster = rebuild(GRaster; name=:G)
-# write("$nc_outDir/G.nc", GRaster)
-# PrecipRaster = rebuild(PrecipRaster; name=:Precip)
-# write("$nc_outDir/Precip.nc", PrecipRaster)
-# UsRaster = rebuild(UsRaster; name=:Us)
-# write("$nc_outDir/Us.nc", UsRaster)
-# GpvRaster = rebuild(GpvRaster; name=:Gpv)
-# write("$nc_outDir/Gpv.nc", GpvRaster)
-# GmeltpvRaster = rebuild(GmeltpvRaster; name=:Gmeltpv)
-# write("$nc_outDir/Gmeltpv.nc", GmeltpvRaster)
-# UpvRaster = rebuild(UpvRaster; name=:Upv)
-# write("$nc_outDir/Upv.nc", UpvRaster)
-# SWEpvRaster = rebuild(SWEpvRaster; name=:SWEpv)
-# write("$nc_outDir/SWEpv.nc", SWEpvRaster)
 
 end_time = time_ns()
 # running_time = (end_time - start_time)/1e9/3600
