8a7d9a16b1e0bb1d14440d2e6cd8b4cb57b60ab0
bidhya <yadav.111@osu.edu>
2024-05-20 21:36:29 -0400

    adaptively select rows based on pix count for slurm job

--- Julia/submit_slurm.jl
+++ Julia/submit_slurm.jl
@@ -28,7 +28,8 @@ if occursin("discover", host_machine) #|| occursin("borg", host_machine)
     hpc_name = "discover"
     cores = 110  # 110 for stepsize 125 for 5 km run.
     if RES == "010"
-        cores = 80  # 60. use less cores to prevent NODE_FAIL error.
+        # for 1km run, use less cores to prevent NODE_FAIL error.
+        cores = 90  # 80. 
     end
     memory = "0" #"184gb"
 elseif occursin(".osc.edu", host_machine)
@@ -108,17 +109,17 @@ function create_job(hpc, jobname, cores, memory, runtime, out_subfolder, start_i
         write(f, "echo ---------------------------------------------------------------------\n")
         write(f, "ls -ltrh\n")
         write(f, "echo -n Count of outputs_txt files: ; ls outputs_txt_*|wc -l\n")  # more specific: outputs_txt_$(start_idx)_$(end_idx)
-        # write(f, "echo -n Logs: ; ls logs_$(start_idx)_$(end_idx)|wc -l\n")  # logs_* only can be a bit misleading because it will match other files with logs prefix, so be explicit.  
-        write(f, "ECODE=\$?\n")
-        write(f, "if [ \$ECODE -eq 0 ]; then\n")
-        write(f, "\ttar -czf logs_$(start_idx)_$(end_idx).tar.gz logs*\n")  # list log files on node
-        write(f, "\tcp logs_$(start_idx)_$(end_idx).tar.gz $base_folder/Runs/$(RES)/$out_subfolder/logs\n")  # folder created at end of call_Blender script  
-        # write(f, "cp logs_$(start_idx)_$(end_idx).tar.gz \$SLURM_SUBMIT_DIR\n")
-        # TODO Write/Call a python script (or Jupyter notebook) to QA/QC the run: count, tables, figures.  
-        write(f, "\techo Finished Slurm job successfully\n\n")
-        write(f, "else\n")
-        write(f, "\techo Error in Blender run\n")
-        write(f, "fi\n")
+        # # write(f, "echo -n Logs: ; ls logs_$(start_idx)_$(end_idx)|wc -l\n")  # logs_* only can be a bit misleading because it will match other files with logs prefix, so be explicit.  
+        # write(f, "ECODE=\$?\n")
+        # write(f, "if [ \$ECODE -eq 0 ]; then\n")
+        # write(f, "\ttar -czf logs_$(start_idx)_$(end_idx).tar.gz logs*\n")  # list log files on node
+        # write(f, "\tcp logs_$(start_idx)_$(end_idx).tar.gz $base_folder/Runs/$(RES)/$out_subfolder/logs\n")  # folder created at end of call_Blender script  
+        # # write(f, "cp logs_$(start_idx)_$(end_idx).tar.gz \$SLURM_SUBMIT_DIR\n")
+        # # TODO Write/Call a python script (or Jupyter notebook) to QA/QC the run: count, tables, figures.  
+        # write(f, "\techo Finished Slurm job successfully\n\n")
+        # write(f, "else\n")
+        # write(f, "\techo Error in Blender run\n")
+        # write(f, "fi\n")
     end
     run(`sbatch $(job_file)`)  # submit the job
 end
@@ -132,29 +133,23 @@ A = A[:SWE_tavg][Ti=1];
 szY = size(A, 2)  # get size in Y-dimension; here dim = 2. 6500 for 1km run.
 # szY = 10 # only for debug and testing, use smaller number
 job_count = 0
-delay_multiplier = 2  # delay the consecutive slurm job by ~2 minutes
+delay_multiplier = 1 # 2. delay the consecutive slurm job by ~2 minutes
 if occursin("asc.ohio-state.edu", host_machine)
     delay_multiplier = 5  # longer delay on unity becuase of slow speeds in moving data (network related)
 end
 total_runtime = 0
+cum_pix_count = 0
+row_count = 0
+start_idx = 1
 # for i in StepRange(3001, step, 3003)  # for testing
+# for i in StepRange(6001, step, szY)
 for i in StepRange(1, step, szY)
-    start_idx = i
-    end_idx = start_idx + step - 1
+    # start_idx = i
+    end_idx = i + step #- 1
     if end_idx > szY
         end_idx = szY
     end
-    # Use next 5 lines only if re-running few folders due to error/timeout. Still testing. 
-    nc_outDir = "$base_folder/Runs/WY$(water_year)/temp_nc/outputs_$(start_idx)_$(end_idx)"
-    if isdir(nc_outDir)  # process only if the pixel is not already processed
-        println("Job Not created because output_folder $nc_outDir already exist")
-        continue
-    end
-
-    # begin_delay = Int((job_count) * step/3)  
-    begin_delay = Int(job_count * delay_multiplier)
-    jobname = "$(start_idx)_$(end_idx)"
-    B = A[1:end, start_idx:end_idx]  # ERROR: LoadError: NetCDF error: NetCDF: Start+count exceeds dimension bound (NetCDF error code: -57)
+    B = A[1:end, i:end_idx]  # ERROR: LoadError: NetCDF error: NetCDF: Start+count exceeds dimension bound (NetCDF error code: -57)
     """
     # TODO Save of copy of sub-array A to hard-drive somewhere. This will by copied by call_blender_v14 script. To prevent slurm jobs failing on Unity.
     # likely due to multiple jobs copying the same file to nodes 
@@ -162,24 +157,47 @@ for i in StepRange(1, step, szY)
     """
     valid_pix_ind = findall(!ismissing, B)
     valid_pix_count = length(valid_pix_ind)
-    # estimate runtime as function of the number of cores used. 270 seconds = 4.5 mins; ie 1 pixel processing time ~ 0.3 min.
-    runtime = Int(ceil(valid_pix_count/(cores*420)))  # hours. 270 for v_15=180; with exclusive, how many cores we get is not certain, but just an estimate
-    runtime = max(runtime, 12)  # bump smaller runtime to 12 hours (max allowed by default on Discover). Due to timeout errors for short runs.   
-    # if runtime < 10 && hpc_name=="discover"
-    #     # keep getting runtime errors when less number of pixels are processed.
-    #     # say in less than 4 hours, likely all cores are not being utilized efficiently
-    #     runtime = 11
-    # end
-    global total_runtime += runtime
-    # 240 seconds : timeout error on OSC, so redude time
-    # runtime = "08:00:00"  # 12 "24:00:00"  # default (max for Discover)
-    # runtime = "$(approx_time):00:00"
-    println(start_idx, " ", end_idx, " ", valid_pix_count, " hours ", runtime)
+    row_count += step
+    cum_pix_count += valid_pix_count
+    if cum_pix_count > 80000 || end_idx == szY  # for last job that man not have enough pixels to pass the pix count threshold
+        # prepare slurm job
+        # begin_delay = Int((job_count) * step/3)  
+        begin_delay = Int(job_count * delay_multiplier)
+        jobname = "$(start_idx)_$(end_idx)"
+        # Use next 5 lines only if re-running few folders due to error/timeout. Still testing. 
+        nc_outDir = "$base_folder/Runs/WY$(water_year)/temp_nc/outputs_$(start_idx)_$(end_idx)"
+        if isdir(nc_outDir)  # process only if the pixel is not already processed
+            println("Job Not created because output_folder $nc_outDir already exist")
+            continue
+        end
+        # estimate runtime as function of the number of cores used. 270 seconds = 4.5 mins; ie 1 pixel processing time ~ 0.3 min.
+        runtime = Int(ceil(valid_pix_count/(cores*420)))  # hours. 270 for v_15=180; with exclusive, how many cores we get is not certain, but just an estimate
+        runtime = max(runtime, 12)  # bump smaller runtime to 12 hours (max allowed by default on Discover). Due to timeout errors for short runs.   
+        # if runtime < 10 && hpc_name=="discover"
+        #     # keep getting runtime errors when less number of pixels are processed.
+        #     # say in less than 4 hours, likely all cores are not being utilized efficiently
+        #     runtime = 11
+        # end
+        global total_runtime += runtime
+        # 240 seconds : timeout error on OSC, so redude time
+        # runtime = "08:00:00"  # 12 "24:00:00"  # default (max for Discover)
+        # runtime = "$(approx_time):00:00"
+        println(start_idx, " ", end_idx, " rows:", row_count, " pixels:", cum_pix_count)  # , " hours ", runtime
+        
+        # create_job(hpc_name, jobname, cores, memory, runtime, "V14x_WY$(water_year)", start_idx, end_idx, valid_pix_count)
+        create_job(hpc_name, jobname, cores, memory, runtime, "WY$(water_year)", start_idx, end_idx, valid_pix_count, begin_delay)
+        # sleep(1)
+        global job_count += 1
+        # reset the counter
+        global row_count = 0
+        global cum_pix_count = 0
+        global start_idx = end_idx + 1
+        if end_idx >= szY
+            println("Exiting loop. End index already included in last job.")
+            break
+        end
     
-    # create_job(hpc_name, jobname, cores, memory, runtime, "V14x_WY$(water_year)", start_idx, end_idx, valid_pix_count)
-    create_job(hpc_name, jobname, cores, memory, runtime, "WY$(water_year)", start_idx, end_idx, valid_pix_count, begin_delay)
-    sleep(1)
-    global job_count += 1
+    end
 end
 println("Total Slurm jobs submitted = $job_count")
 println("Total Runtime = $total_runtime")
