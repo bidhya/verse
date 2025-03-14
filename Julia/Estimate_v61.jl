4cfc944ab5e8779ca03fc4646d69bba1b4d04b84
bidhya <yadav.111@osu.edu>
2025-03-13 23:15:23 -0400

    cleanup and formatting

--- Julia/Estimate_v61.jl
+++ Julia/Estimate_v61.jl
@@ -15,60 +15,60 @@ function blender(i, j, SWEprior, Pprior, Gprior, SCFinst, AirT, logDir, exp_dir)
 
   """
     # 0 handle variable sizes
-    nt=length(SCFinst)
-    Pprior=Pprior[1:nt-1]
+    nt = length(SCFinst)
+    Pprior = Pprior[1:nt-1]
     
     # 1 Smooth SCF observations    
-    twindow=5
-    SCFobs=smoothdata(SCFinst,twindow,nt,"mean");    
-    twindow=60
-    SCF_smooth_season=smoothdata(SCFinst,twindow,nt,"mean");
+    twindow = 5
+    SCFobs = smoothdata(SCFinst,twindow,nt,"mean");    
+    twindow = 60
+    SCF_smooth_season = smoothdata(SCFinst,twindow,nt,"mean");
 
     # 2 Define hyperparameters
     # 2.1 Define prior estimates
     # 2.1.1 Define times when snow is melting 
-    tmelt=zeros(nt,1)
-    ΔSCFthresh=-0.01
+    tmelt = zeros(nt,1)
+    ΔSCFthresh = -0.01
     for i=2:nt
         if SCF_smooth_season[i]-SCF_smooth_season[i-1] < ΔSCFthresh
-            tmelt[i]=1
+            tmelt[i] = 1
         end
     end    
-    twindow=30
-    tmelt_smooth=smoothdata(tmelt,twindow,nt,"mean");
+    twindow = 30
+    tmelt_smooth = smoothdata(tmelt,twindow,nt,"mean");
     
     # 2.2 Extreme / limit values
     # 2.2.1 SWE
-    SWEmax_global=5;
-    SWEmin_global=1.0e-6; #1/1000 mm
+    SWEmax_global = 5;
+    SWEmin_global = 1.0e-6; #1/1000 mm
     # define SWEmax as a function of time and of SCF
     #    set SWEmax to 0 if SCF is low
-    SWEmax=zeros(nt,1)
-    for i=1:nt
-      if SCFobs[i]==0
-        SWEmax[i]=SWEmin_global
+    SWEmax = zeros(nt,1)
+    for i = 1:nt
+      if SCFobs[i] == 0
+        SWEmax[i] = SWEmin_global
       else
-        SWEmax[i]=SWEmax_global
+        SWEmax[i] = SWEmax_global
       end
     end    
     #2.2.2 Melt
-    Meltmax=0.075;
+    Meltmax = 0.075;
     
     # 2.3 Define uncertainty
-    σP,σSWE=define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
+    σP,σSWE = define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
     # 2.4 Melt cost function parameters
-    k=500
-    Melt0=0.05
-    L=1
+    k = 500
+    Melt0 = 0.05
+    L = 1
 
     # 3 Solve
     m = Model(optimizer_with_attributes(Ipopt.Optimizer,"max_iter"=>5000))
     # set_silent(m)
     # define variables and bounds
-    @variable(m, SWEmin_global <= SWE[i=1:nt] <= SWEmax[i],start=SWEprior[i] );
-    @variable(m,  Precip[i=1:nt-1]>=0. ,start=Pprior[i]);
-    @variable(m,   0. <= Melt[i=1:nt-1]<= Meltmax );
-    @variable(m, Mcost[i=1:nt-1] >=0);    
+    @variable(m, SWEmin_global <= SWE[i=1:nt] <= SWEmax[i], start=SWEprior[i]);
+    @variable(m, Precip[i=1:nt-1]>=0. ,start=Pprior[i]);
+    @variable(m, 0. <= Melt[i=1:nt-1]<= Meltmax);
+    @variable(m, Mcost[i=1:nt-1] >=0);
     # define constraints
     for i in 1:nt-1
       @NLconstraint(m,Mcost[i]==L/(1+exp( -k*(Melt[i]-Melt0)   ) ))
@@ -83,27 +83,27 @@ function blender(i, j, SWEprior, Pprior, Gprior, SCFinst, AirT, logDir, exp_dir)
     redirect_stdio(stdout=log_file, stderr=log_file) do
       optimize!(m)
     end
-    print(termination_status(m))
+    # print(termination_status(m))
     
     # 4 extract
-    NODATAvalue=-9999
+    NODATAvalue = -9999
     SWEhat=JuMP.value.(SWE);
     Phat=zeros(nt,1)
-    Phat[1:nt-1]=JuMP.value.(Precip);           
-    Melt_hat=zeros(nt,1)
-    Melt_hat[1:nt-1]=JuMP.value.(Melt);
+    Phat[1:nt-1] = JuMP.value.(Precip);           
+    Melt_hat = zeros(nt,1)
+    Melt_hat[1:nt-1] = JuMP.value.(Melt);
     
-    Δt=86400; #s/day
-    ρw=1000; #density of water
-    Lf=0.334E6; #Latent heat of fusion J/kg    
-    GmeltHat=Melt_hat/Δt*Lf*ρw
+    Δt = 86400; #s/day
+    ρw = 1000; #density of water
+    Lf = 0.334E6; #Latent heat of fusion J/kg    
+    GmeltHat = Melt_hat/Δt*Lf*ρw
     
-    Ghat=ones(nt,1)*NODATAvalue;    
-    Ushat=ones(nt,1)*NODATAvalue;
-    G_pv=ones(nt,1)*NODATAvalue;
-    U_pv=ones(nt,1)*NODATAvalue;
-    Gmelt_pv=ones(nt,1)*NODATAvalue;
-    SWEpv=ones(nt,1)*NODATAvalue;
+    Ghat = ones(nt,1)*NODATAvalue;    
+    Ushat = ones(nt,1)*NODATAvalue;
+    G_pv = ones(nt,1)*NODATAvalue;
+    U_pv = ones(nt,1)*NODATAvalue;
+    Gmelt_pv = ones(nt,1)*NODATAvalue;
+    SWEpv = ones(nt,1)*NODATAvalue;
     
     # 5 output
     out_vars = hcat(SWEhat, GmeltHat, Ghat, Phat, Ushat, G_pv, Gmelt_pv, U_pv, SWEpv, SCFobs)
@@ -118,16 +118,16 @@ end
 function smoothdata(SCF_inst,twindow,nt,smoothfunc)
     SCF_smooth=zeros(nt,1);
     for i=1:nt
-        istart=trunc(Int,i-round(twindow/2))
-        iend=trunc(Int,i+round(twindow/2))        
+        istart = trunc(Int,i-round(twindow/2))
+        iend = trunc(Int,i+round(twindow/2))        
         # if i < twindow || i > nt-twindow
         if istart < 1 || iend > nt
             SCF_smooth[i]=0
         else            
-            if smoothfunc=="mean"
-                SCF_smooth[i]=mean(SCF_inst[istart:iend] )
+            if smoothfunc == "mean"
+                SCF_smooth[i] = mean(SCF_inst[istart:iend])
             elseif smoothfunc=="median"
-                SCF_smooth[i]=median(SCF_inst[istart:iend] )
+                SCF_smooth[i] = median(SCF_inst[istart:iend])
             end
         end
     end
@@ -136,16 +136,15 @@ function smoothdata(SCF_inst,twindow,nt,smoothfunc)
 end
 
 function define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
-
     # convert air temperature C-> K
     AirT=AirT.-273.15
     
     # 2.2.1 Precipitation Uncertainty
-    RelPUnc=0.3; #[-] this applies to cumulative precipitation
+    RelPUnc = 0.3; #[-] this applies to cumulative precipitation
     # Uncertainty for accumulation . precip is size nt-1
-    σP=zeros(nt-1,1)
-    σPmin=0.001
-    Pmin=0.001
+    σP = zeros(nt-1,1)
+    σPmin = 0.001
+    Pmin = 0.001
     for i=1:nt-1
       if Pprior[i]<Pmin
         σP[i]=σPmin
@@ -154,32 +153,31 @@ function define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
       end
     end    
     # adjust uncertainty to apply to the number of snow days
-    nsnowday=0
-    Pmin=0.001
+    nsnowday = 0
+    Pmin = 0.001
     for i=1:nt-1
         if Pprior[i]>0.001 && AirT[i] < 1.5
-            nsnowday+=1
+            nsnowday += 1
         end
     end
     if nsnowday > 0
-        σP=σP*sqrt(nsnowday);    
+        σP = σP*sqrt(nsnowday);    
     end
     
     # 2.2.2 SWE Uncertainty
-    fSWE=0.4
-    σSWE=SWEprior*fSWE;
-    σSWEmin=0.01
-    σSWEmax=10
+    fSWE = 0.4
+    σSWE = SWEprior*fSWE;
+    σSWEmin = 0.01
+    σSWEmax = 10
     for i=1:nt
         # if SWEprior[i]>0 && SCFobs[i]==0
-        if SCFobs[i]==0    || tmelt_smooth[i]>.1    
-            σSWE[i]=σSWEmax
+        if SCFobs[i]==0 || tmelt_smooth[i]>.1    
+            σSWE[i] = σSWEmax
         end
-
-        if σSWE[i]<σSWEmin
-            σSWE[i]=σSWEmin
+        if σSWE[i] < σSWEmin
+            σSWE[i] = σSWEmin
         end
     end   
     
-    return σP,σSWE
+    return σP, σSWE
 end
