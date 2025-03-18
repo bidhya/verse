c47ff0f1af0f7e30e9e5f572f3a1dbd61d6abc6b
mikedurand <durand.8@osu.edu>
2025-03-18 13:15:25 -0400

    Update Estimate_v61.jl
    Refactored hyperparameter lines into functions. Improved documentation

--- Julia/Estimate_v61.jl
+++ Julia/Estimate_v61.jl
@@ -6,6 +6,14 @@ Random.seed!(1234)  # seed for reproducibility
 
 function blender(i, j, SWEprior, Pprior, Gprior, SCFinst, AirT, logDir, exp_dir)
   """
+  inputs
+  ==============
+  i,j are pixel locations
+  SWEprior, Pprior, Gprior are prior estimates of SWE [m], Precipitation [m] and surface heat flux [W/m2]
+  SCFinst is measured snow covered fraction
+  AirT is air temperature [K]
+  logdir and exp_dir are directories where log and data are written
+    
   variable sizes
   =============== 
   SWE and SCF are storage terms, so will be length nt. 
@@ -25,41 +33,7 @@ function blender(i, j, SWEprior, Pprior, Gprior, SCFinst, AirT, logDir, exp_dir)
     SCF_smooth_season = smoothdata(SCFinst,twindow,nt,"mean");
 
     # 2 Define hyperparameters
-    # 2.1 Define prior estimates
-    # 2.1.1 Define times when snow is melting 
-    tmelt = zeros(nt,1)
-    ΔSCFthresh = -0.01
-    for i=2:nt
-        if SCF_smooth_season[i]-SCF_smooth_season[i-1] < ΔSCFthresh
-            tmelt[i] = 1
-        end
-    end    
-    twindow = 30
-    tmelt_smooth = smoothdata(tmelt,twindow,nt,"mean");
-    
-    # 2.2 Extreme / limit values
-    # 2.2.1 SWE
-    SWEmax_global = 5;
-    SWEmin_global = 1.0e-6; #1/1000 mm
-    # define SWEmax as a function of time and of SCF
-    #    set SWEmax to 0 if SCF is low
-    SWEmax = zeros(nt,1)
-    for i = 1:nt
-      if SCFobs[i] == 0
-        SWEmax[i] = SWEmin_global
-      else
-        SWEmax[i] = SWEmax_global
-      end
-    end    
-    #2.2.2 Melt
-    Meltmax = 0.075;
-    
-    # 2.3 Define uncertainty
-    σP,σSWE = define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
-    # 2.4 Melt cost function parameters
-    k = 500
-    Melt0 = 0.05
-    L = 1
+    tmelt,tmelt_smooth,SWEmax,SWEmin_global,Meltmax,σP,σSWE,k,Melt0,L=define_hyperparameters(SCF_smooth_season,nt,Pprior,SWEprior,AirT)
 
     # 3 Solve
     m = Model(optimizer_with_attributes(Ipopt.Optimizer,"max_iter"=>5000))
@@ -136,7 +110,7 @@ function smoothdata(SCF_inst,twindow,nt,smoothfunc)
 end
 
 function define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
-    # convert air temperature C-> K
+    # convert air temperature K-> C
     AirT=AirT.-273.15
     
     # 2.2.1 Precipitation Uncertainty
@@ -144,9 +118,9 @@ function define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
     # Uncertainty for accumulation . precip is size nt-1
     σP = zeros(nt-1,1)
     σPmin = 0.001
-    Pmin = 0.001
+    Pthresh = 0.001
     for i=1:nt-1
-      if Pprior[i]<Pmin
+      if Pprior[i]<Pthresh
         σP[i]=σPmin
       else
         σP[i]=Pprior[i]*RelPUnc
@@ -154,9 +128,9 @@ function define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
     end    
     # adjust uncertainty to apply to the number of snow days
     nsnowday = 0
-    Pmin = 0.001
+    Tprecip_thresh=1.5
     for i=1:nt-1
-        if Pprior[i]>0.001 && AirT[i] < 1.5
+        if Pprior[i]>Pthresh && AirT[i] < Tprecip_thresh
             nsnowday += 1
         end
     end
@@ -180,4 +154,64 @@ function define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
     end   
     
     return σP, σSWE
+end
+
+function define_hyperparameters(SCF_smooth_season,nt,Pprior,SWEprior,AirT)
+    """
+    list of hyperparameters
+    =======================
+    twindow for smoothing SCF for snow on/off constraint - defined in main
+    twindow for smoothing SCF for identifying melt times - defined in main
+    ΔSCFthresh
+    twindow for smoothing melt times
+    SWEmax_global
+    SWEmin_global
+    Meltmax    
+    k,Melt0,L
+    σPmin - defined in define_uncertainty
+    Pthresh -  defined in define_uncertainty
+    Tprecip_thresh -  defined in define_uncertainty
+    fSWE -  defined in define_uncertainty
+    σSWEmin -  defined in define_uncertainty
+    σSWEmax -  defined in define_uncertainty        
+    
+    """
+    
+    # 2.1 Define prior estimates
+    # 2.1.1 Define times when snow is melting 
+    tmelt = zeros(nt,1)
+    ΔSCFthresh = -0.01
+    for i=2:nt
+        if SCF_smooth_season[i]-SCF_smooth_season[i-1] < ΔSCFthresh
+            tmelt[i] = 1
+        end
+    end    
+    twindow = 30
+    tmelt_smooth = smoothdata(tmelt,twindow,nt,"mean");
+    
+    # 2.2 Extreme / limit values
+    # 2.2.1 SWE
+    SWEmax_global = 5;
+    SWEmin_global = 1.0e-6; #1/1000 mm
+    # define SWEmax as a function of time and of SCF
+    #    set SWEmax to 0 if SCF is low
+    SWEmax = zeros(nt,1)
+    for i = 1:nt
+      if SCFobs[i] == 0
+        SWEmax[i] = SWEmin_global
+      else
+        SWEmax[i] = SWEmax_global
+      end
+    end    
+    #2.2.2 Melt
+    Meltmax = 0.075;
+    
+    # 2.3 Define uncertainty
+    σP,σSWE = define_uncertainty(Pprior,SWEprior,AirT,SCFobs,nt,tmelt_smooth)
+    # 2.4 Melt cost function parameters
+    k = 500
+    Melt0 = 0.05
+    L = 1
+    
+    return tmelt,tmelt_smooth,SWEmax,SWEmin_global,Meltmax,σP,σSWE,k,Melt0,L
 end
